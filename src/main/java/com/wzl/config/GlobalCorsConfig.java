328db9a87ca3c8f321bbf2e6e1d88259679f0f42
禁止种太阳 <571241210@qq.com>
2023-01-03 21:24:49 +0800

    [Fix] book
    Signed-off-by: 禁止种太阳 <571241210@qq.com>

--- src/main/java/com/wzl/config/GlobalCorsConfig.java
+++ src/main/java/com/wzl/config/GlobalCorsConfig.java
@@ -12,7 +12,7 @@ public class GlobalCorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         // 设置你要允许的网站域名
-        config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedOrigin("http://49.235.71.95:8081");
         //允许跨域发送cookie
         config.setAllowCredentials(true);
         //放行全部原始头信息
